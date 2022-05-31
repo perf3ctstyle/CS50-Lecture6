3d1781d8b2ceacb4f967b4b9dc7c2120636e8517
nikitatorop <torop.nikita1@gmail.com>
2022-05-31 09:24:01 +0300

    Add sum

---
+++ calculator.py
@@ -0,0 +1,10 @@
+import cs50
+
+if __name__ == '__main__':
+    x = cs50.get_int("x: ")
+    y = cs50.get_int("y: ")
+    print(x + y)
+
+    a = int(input("a: "))
+    b = int(input("b: "))
+print(a + b)
