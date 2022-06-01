aadae63a2308c52e85e1baadbe736ee13bbf7d03
nikitatorop <torop.nikita1@gmail.com>
2022-06-01 07:09:35 +0300

    Add conditions based on string user input

---
+++ agree.py
@@ -0,0 +1,9 @@
+from cs50 import get_string
+
+if __name__ == "__main__":
+    s = get_string("Do you agree? ").lower()
+
+    if s in ["y", "yes"]:
+        print("Agreed.")
+    elif s in ["n", "no"]:
+        print("Not agreed.")
