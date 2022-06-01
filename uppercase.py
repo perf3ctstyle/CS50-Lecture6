6b19256a49be90bcc1c9d82d2cdfa7acf265a938
nikitatorop <torop.nikita1@gmail.com>
2022-06-01 09:43:35 +0300

    Extract uppercasing into own function, add function print string from index

--- uppercase.py
+++ uppercase.py
@@ -1,10 +1,26 @@
+def uppercase_char_by_char(string):
+    i = 0
+    new_string = ""
+    for c in string:
+        c = c.upper()
+        new_string += c
+        i += 1
+    return new_string
+
+
+def print_string_from(string, index):
+    for c in string[index:]:
+        print(c, end="")
+    print()
+
+
 if __name__ == "__main__":
     before = input("Input a string: ")
-    print(f"After: ", end="")
-    for c in before:
-        print(c.upper(), end="")
-    print()
+    after = uppercase_char_by_char(before)
+    print(f"After: {after}")
 
     before = input("Input another string: ")
     after = before.upper()
     print(f"After: {after}")
+
+    print_string_from(after, 2)
