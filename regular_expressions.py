d2f9f069b599e725a5ba9fd2bb7315d6d688e986
nikitatorop <torop.nikita1@gmail.com>
2022-06-02 09:04:57 +0300

    Add support for arbitrary title that user types in

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,14 +1,17 @@
 import csv
 import re
 
+title = input("Title: ").strip().upper()
 counter = 0
 
 if __name__ == "__main__":
     with open("favorites.csv", "r") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            title = row["title"].strip().upper()
-            if re.search("^(OFFICE|THE.OFFICE)$", title):
+            current_title = row["title"].strip().upper()
+
+            # easier to check equality with == so that's just solely for practice
+            if re.search("^" + "(" + title + ")" + "$", current_title):
                 counter += 1
 
-    print(f"Number of people who like the Office: {counter}")
+    print(f"Number of people who like {title}: {counter}")
