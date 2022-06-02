7fd34353f0c91e520b9ecb0135f0d3dff84ce8a6
nikitatorop <torop.nikita1@gmail.com>
2022-06-02 07:13:24 +0300

    Add lambda instead of get_title function

--- favorites.py
+++ favorites.py
@@ -2,11 +2,6 @@ import csv
 
 titles = dict()
 
-
-def get_value(title):
-    return titles[title]
-
-
 if __name__ == "__main__":
     with open("favorites.csv", "r") as file:
         reader = csv.DictReader(file)
@@ -16,5 +11,5 @@ if __name__ == "__main__":
                 titles[title] = 0
             titles[title] += 1
 
-    for title in sorted(titles, key=get_value, reverse=True):
-        print(title)
+    for title in sorted(titles, key=lambda title: titles[title], reverse=True):
+        print(title, titles[title])
